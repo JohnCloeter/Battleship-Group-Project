f214ee34ce734e7eb3cc47f92d68ed9afbd5f481
zmpeabody <zmpeabody77@gmail.com>
2017-05-02 15:09:14 -0500

    Update Ship.h

--- Ship.h
+++ Ship.h
@@ -1,43 +1,21 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Ship//Class ship for the main function to call the ship class.
 {
-	int length;
-	string shipname;
-	
-
-	bool damage [] = { -1 };
-
 
 public:
-	void Ship::setlength(int len);
-	void Ship::setshipname(string name);
-
-	bool setlength, setship = false;
+	Ship(string name, int length)
+	int getLength();
+	string getName();
+	int hitShip();
 
 private:
-	int get_len;
-	string get_nam;
-
-
-
+	int _length;
+	string _name;
+	std::vector<bool> damage;
+	
 
 };
-
-
-
-void setlength()
-{
-	int len;
-
-
-
-}
-
-void setshipname()
-{
-	string nam;
-
-}
